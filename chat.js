f1ec8f94b72cfea032ee6690d9d9cc7ed6133cfe
Dwijen <dwijen.bapi@gmail.com>
2013-07-09 20:03:56 +0530

    up and running

---
+++ chat.js
@@ -0,0 +1,9 @@
+var net= require('net');
+var chatServer = net.createServer();
+chatServer.on('connection',function(client){
+client.write('Hi!\n');
+client.write('bye!\n');
+client.end();
+});
+
+chatServer.listen(9000);
